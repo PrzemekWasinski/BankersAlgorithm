bc1bed02942113ea99f3a2f7850a9975e0ad7019
PrzemekWasinski <przemek282005@outlook.com>
2025-02-20 13:15:25 +0000

    Added allocation matrix

--- bankers.sh
+++ bankers.sh
@@ -5,9 +5,11 @@ read -p "Enter number of processes: " process_amount
 read -p "Enter number of resource types: " resource_amount
 
 declare -A processes
+declare -A allocation_matrix
 
 available=()
 
+#
 for ((i = 0; i < process_amount; i++)); do
 	for ((j = 0; j < resource_amount; j++)); do
 		read -p "Enter Max Resource Request R$j for Process P$i: " resource_request
@@ -15,19 +17,45 @@ for ((i = 0; i < process_amount; i++)); do
 	done
 done
 
+echo "Processes matrix: "
+echo
+
 echo -n "    "
 for ((i = 0; i < resource_amount; i++)); do
 	echo -n	"R$i "
 done
 
 echo
-
 for ((i = 0; i < process_amount; i++)); do
 	echo -n "P$i: "
 	for ((j = 0; j < resource_amount; j++)); do
 		echo -n "${processes[$i, $j]}  "
 	done
-	echo " "
+	echo
+done
+
+for ((i = 0; i < process_amount; i++)); do
+	for ((j = 0; j < resource_amount; j++)); do
+		allocation_matrix[$i, $j]=$(( ( RANDOM % 5 )  + 1 ))
+	done
+done
+
+echo
+echo "Allocation Matrix:"
+echo
+
+echo -n "   "
+for ((i = 0; i < resource_amount; i++)); do
+	echo -n "R$i "
+done
+
+echo
+for ((i = 0; i < process_amount; i++)); do
+	echo -n "P$i: "
+	for ((j = 0; j < resource_amount; j++)); do
+		echo -n "${allocation_matrix[$i, $j]} "
+	done
+	echo
 done
 
 for ((i = 0; i < resource_amount; i++)); do
@@ -35,6 +63,15 @@ for ((i = 0; i < resource_amount; i++)); do
 	available[i]="$available_resource"
 done
 
-echo "Available array: [${available[@]}]"
+echo "Available array: ${available[@]}"
 
+for ((i = 0; i < process_amount; i++)); do
+	for ((j = 0; j < resource_amount; j++)); do
+		if [[ ${available[$j]} -ge ${processes[$i, $j]} ]]; then
+			echo "yes"
+		else
+			echo "no"
+		fi
+	done
+done
 
